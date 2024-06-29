ccc81bfa95efd5eb258f519ba9068bc4c256895f
Solito Reyes III <solrey3@solrey3.com>
2024-06-29 19:39:30 -0400

    update debian dockerfile and terraform

--- builds/debian/main.tf
+++ builds/debian/main.tf
@@ -31,9 +31,9 @@ resource "digitalocean_droplet" "workstation" {
     inline = [
       "apt-get update",
       "apt-get install -y sudo",
-      "curl -O https://raw.githubusercontent.com/solrey3/dotfiles/main/builds/debian/setup.sh",
-      "chmod +x setup.sh",
-      "./setup.sh"
+      "curl -O https://raw.githubusercontent.com/solrey3/dotfiles/main/builds/debian/start.sh",
+      "chmod +x start.sh",
+      "./start.sh"
     ]
   }
 }
