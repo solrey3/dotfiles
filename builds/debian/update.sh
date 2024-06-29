b7bc1d2bbd7223c0f49217bfac81d6009ed306c6
Solito Reyes III <solrey3@solrey3.com>
2024-06-29 18:40:46 -0400

    fix alacritty, include stow

--- builds/debian/update.sh
+++ builds/debian/update.sh
@@ -1,86 +1,32 @@
 #!/bin/bash
 
-# Clone or update the dotfiles repository
-DOTFILES_REPO="https://github.com/solrey3/dotfiles"
-DOTFILES_DIR="$HOME/dotfiles"
-
-if [ -d "$DOTFILES_DIR" ]; then
-	echo "Updating dotfiles repository..."
-	cd "$DOTFILES_DIR" && git pull
-else
-	echo "Cloning dotfiles repository..."
-	git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
-fi
-
 # Update starship
 echo "Updating starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 
-# Define dotfiles config directory
-DOTFILES_CONFIG_DIR="$HOME/dotfiles/"
+# Install stow if not already installed
+if ! command -v stow &>/dev/null; then
+	echo "stow could not be found, installing..."
+	sudo apt update
+	sudo apt install -y stow
+fi
 
-# Create the .config directory if it doesn't exist
-mkdir -p ~/.config
-mkdir -p ~/.config/nvim
+# Define dotfiles directory
+DOTFILES_DIR="$HOME/dotfiles"
 
-# Fix locale issue
-sudo locale-gen en_US.UTF-8
-sudo bash -c 'echo -e "LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8" > /etc/default/locale'
-source /etc/default/locale
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# Create .stow-local-ignore file to exclude certain files and directories
+cat <<EOL >$DOTFILES_DIR/.stow-local-ignore
+.git
+builds
+README.md
+LICENSE
+EOL
 
-# Remove bind commands from .bashrc
-sed -i '/bind/d' "$DOTFILES_CONFIG_DIR/bash/.bashrc"
+# Use stow to create symlinks for all configurations
+stow -d "$DOTFILES_DIR" -t "$HOME" .
 
-# Function to create symbolic links and backup existing regular files
-create_symlink() {
-	local target=$1
-	local link=$2
+# Install Neovim plugins using LazyVim
+echo "Installing Neovim plugins..."
+nvim --headless -c 'lua require("lazy").sync()' +qa
 
-	if [ -e "$link" ] && [ ! -L "$link" ]; then
-		mv "$link" "$link.backup"
-		echo "Backed up existing $link to $link.backup"
-	fi
-
-	ln -sf "$target" "$link"
-	echo "Created symlink: $link -> $target"
-}
-
-# Function to copy files and backup existing regular files
-copy_file() {
-	local target=$1
-	local destination=$2
-
-	if [ -e "$destination" ] && [ ! -L "$destination" ]; then
-		mv "$destination" "$destination.backup"
-		echo "Backed up existing $destination to $destination.backup"
-	fi
-
-	rsync -a "$target" "$destination"
-	echo "Copied $target to $destination"
-}
-
-# Symlink bash configuration
-create_symlink "$DOTFILES_CONFIG_DIR/.bashrc" "$HOME/.bashrc"
-create_symlink "$DOTFILES_CONFIG_DIR/.bash_profile" "$HOME/.bash_profile"
-
-# Copy tmux configuration
-# copy_file "$DOTFILES_CONFIG_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
-create_syslink "$DOTFILES_CONFIG_DIR/.tmux.conf" "$HOME/.tmux.conf"
-create_symlink "$DOTFILES_CONFIG_DIR/tmux_startup.sh" "$HOME/tmux_startup.sh"
-
-# Symlink alacritty configuration
-create_symlink "$DOTFILES_CONFIG_DIR/.config/alacritty.yml" "$HOME/.config/alacritty.yml"
-
-# Symlink vim configuration
-create_symlink "$DOTFILES_CONFIG_DIR/.vimrc" "$HOME/.vimrc"
-
-# Copy all files and directories in nvim directory
-copy_file "$DOTFILES_CONFIG_DIR/.config/nvim/" "$HOME/.config/nvim"
-
-# Symlink starship configuration
-create_symlink "$DOTFILES_CONFIG_DIR/.config/starship.toml" "$HOME/.config/starship.toml"
-
-# Source the .bashrc to apply changes
-source $HOME/.bashrc
+echo "Update complete."
--- builds/macos/update.sh
+++ builds/macos/update.sh
@@ -7,63 +7,19 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 # Define dotfiles directory
 DOTFILES_DIR="$HOME/dotfiles"
 
-# Create the .config directory if it doesn't exist
-mkdir -p ~/.config
-mkdir -p ~/.config/nvim
+# Create .stow-local-ignore file to exclude certain files and directories
+cat <<EOL >$DOTFILES_DIR/.stow-local-ignore
+.git
+builds
+README.md
+LICENSE
+EOL
 
-# Function to create symbolic links and backup existing regular files
-create_symlink() {
-	local target=$1
-	local link=$2
-
-	if [ -e "$link" ] && [ ! -L "$link" ]; then
-		mv "$link" "$link.backup"
-		echo "Backed up existing $link to $link.backup"
-	fi
-
-	ln -sf "$target" "$link"
-	echo "Created symlink: $link -> $target"
-}
-
-# Function to copy files and backup existing regular files
-copy_file() {
-	local target=$1
-	local destination=$2
-
-	if [ -e "$destination" ] && [ ! -L "$destination" ]; then
-		mv "$destination" "$destination.backup"
-		echo "Backed up existing $destination to $destination.backup"
-	fi
-
-	rsync -a "$target" "$destination"
-	echo "Copied $target to $destination"
-}
-
-# Symlink bash configuration
-create_symlink "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
-create_symlink "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
-
-# Symlink zsh configuration
-create_symlink "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-
-# Copy tmux configuration
-create_symlink "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
-create_symlink "$DOTFILES_DIR/tmux_startup.sh" "$HOME/tmux_startup.sh"
-
-# Symlink alacritty configuration
-create_symlink "$DOTFILES_DIR/.config/alacritty.yml" "$HOME/.config/alacritty.yml"
-
-# Symlink vim configuration
-create_symlink "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-
-# Copy all files and directories in nvim directory
-copy_file "$DOTFILES_DIR/.config/nvim/" "$HOME/.config/nvim"
-
-# Symlink starship configuration
-create_symlink "$DOTFILES_DIR/.config/starship.toml" "$HOME/.config/starship.toml"
+# Use stow to create symlinks for all configurations
+stow -d "$DOTFILES_DIR" -t "$HOME" .
 
 # Install Neovim plugins using LazyVim
 echo "Installing Neovim plugins..."
-nvim --headless "+Lazy sync" +qa
+nvim --headless -c 'lua require("lazy").sync()' +qa
 
 echo "Update complete."
