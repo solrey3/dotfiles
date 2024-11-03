272498528971ed115c5c15eff9b90dbd91a4fe9a
Buddha Christ <budchris@solrey3.com>
2024-11-03 18:58:35 -0500

    refactor and update stow

---
+++ stow_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Stow alacritty configuration
+stow -t ~/.config/alacritty alacritty
+
+# Stow bash configuration
+stow -t ~ bash
+
+# Stow nix configuration
+stow -t /etc/nix nix
+
+# Stow nvim configuration
+stow -t ~/.config/nvim nvim
+
+# Stow starship configuration
+stow -t ~/.config starship
+
+# Stow tmux configuration
+stow -t ~ tmux
+
+# Stow vim configuration
+stow -t ~ vim
+
+# Stow zsh configuration
+stow -t ~ zsh
+
+echo "All files have been stowed to their expected locations."
